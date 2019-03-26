8b415bedee8b857b33e10aa6c3f4bf9d6b6830a6
Jose Angel Riveaux <jangel.riveaux@gmail.com>
2019-03-26 11:18:47 -0300

    adding script

---
+++ src/scripts/worker1.sh
@@ -0,0 +1,10 @@
+
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+cd "$parent_path"
+
+date=$(date +%Y%m%d)
+
+now=$(date +"%T")
+
+curl  https://n1.doorig.com/visibility/src/index.php/test/worker_test/test >> /opt/lampp/htdocs/visibility/logs/worker-${date}.log
