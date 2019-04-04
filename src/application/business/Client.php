69da5212ba8fdf942b38c7f43784cff1e29a196a
Jose Angel Riveaux <jangel.riveaux@gmail.com>
2019-04-04 17:19:08 -0300

    checkpoint problem in process

--- src/application/business/Client.php
+++ src/application/business/Client.php
@@ -144,8 +144,6 @@ namespace business {
                 $ci->session->set_userdata('client', serialize($this));
                 $cookies_str = json_encode($login_response->Cookies);
                 self::update($this->Id, null, null, null, null, null, null, null, null, null, $cookies_str);
-                $login_response->code = 0;
-                $login_response->message = "OK";
             }
 
             return $login_response;
